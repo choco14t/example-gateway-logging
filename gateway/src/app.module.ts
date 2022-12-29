c0557483cefd4de4aad4ffdb1e6414ac1bc474b1
choco <8362391+choco14t@users.noreply.github.com>
2022-12-30 04:55:32 +0900

    feat: setup gateway

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,10 +1,35 @@
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { v4 } from 'uuid';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        cors: true,
+        context: () => {
+          return { requestId: v4() };
+        },
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'posts', url: 'http://localhost:3001/graphql' },
+            { name: 'users', url: 'http://localhost:3002/graphql' },
+          ],
+        }),
+        buildService: ({ url }) =>
+          new RemoteGraphQLDataSource<{ requestId: string }>({
+            url,
+            willSendRequest: ({ request, context }) => {
+              request.http.headers.set('x-request-id', context.requestId);
+            },
+          }),
+      },
+    }),
+  ],
 })
 export class AppModule {}
