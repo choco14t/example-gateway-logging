e4ad8b32e5f7380427bc6890e448a136ecb2486b
choco <8362391+choco14t@users.noreply.github.com>
2022-12-30 09:40:01 +0900

    feat: apply logging

--- application-posts/src/app.module.ts
+++ application-posts/src/app.module.ts
@@ -6,6 +6,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { User } from './posts/dto/user';
 import { PostsModule } from './posts/posts.module';
+import { LoggingPlugin } from './shared/plugins/logging.plugin';
 
 @Module({
   imports: [
@@ -19,5 +20,6 @@ import { PostsModule } from './posts/posts.module';
     }),
     PostsModule,
   ],
+  providers: [LoggingPlugin],
 })
 export class AppModule {}
---
+++ application-posts/src/shared/plugins/logging.plugin.ts
@@ -0,0 +1,28 @@
+import { Plugin } from '@nestjs/apollo';
+import { ExpressContext } from 'apollo-server-express';
+import {
+  ApolloServerPlugin,
+  GraphQLRequestContext,
+  GraphQLRequestListener,
+} from 'apollo-server-plugin-base';
+
+@Plugin()
+export class LoggingPlugin implements ApolloServerPlugin {
+  async requestDidStart(
+    requestContext: GraphQLRequestContext<ExpressContext>,
+  ): Promise<GraphQLRequestListener<ExpressContext>> {
+    return {
+      willSendResponse: async ({ context }) => {
+        const {
+          request: { query, variables },
+        } = requestContext;
+        console.log({
+          query,
+          variables: JSON.stringify(variables),
+          service: 'posts',
+          request_id: context.req.headers['x-request-id'],
+        });
+      },
+    };
+  }
+}
