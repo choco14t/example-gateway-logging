32de3b68469b616b7040b14a8d2eced3e39c83fa
choco <8362391+choco14t@users.noreply.github.com>
2022-12-30 09:50:03 +0900

    Implement users application (#3)
    * chore: add packages
    
    * feat: implement user queries
    
    * chore: add apollo-server-plugin-base
    
    * feat: apply logging

--- application-users/src/app.module.ts
+++ application-users/src/app.module.ts
@@ -1,10 +1,21 @@
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LoggingPlugin } from './shared/plugins/logging.plugin';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: true,
+      sortSchema: true,
+    }),
+    UsersModule,
+  ],
+  providers: [LoggingPlugin],
 })
 export class AppModule {}
